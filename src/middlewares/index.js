651f0d444eb66133bde3ed017bec2eee168a7fb6
Maicon <hrqn.fernandes@gmail.com>
2021-07-08 15:36:55 -0300

    Criada autenticação simples e proteção de rostas específicas de post/update.

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,5 +1,16 @@
 const middlewares = {
 
+    authenticate(req, res, next){
+
+        const {authentication} = req.headers
+        const { user_id } = req.params
+
+        if (!authentication)  return res.status(400).json({message: 'No authentication was found!'})
+        if (authentication != user_id)  return res.status(400).json({message: 'Access denied!'})
+
+        next()
+        
+    }
 }
 
 module.exports = middlewares
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,8 @@ const UserController = require('../controllers/UserController')
 const LoginController = require('../controllers/Login')
 const ProductController = require('../controllers/ProductController')
 const CartController = require('../controllers/CartController')
+
+const { authenticate } = require('../middlewares')
 const routes = Router()
 
 
@@ -19,14 +21,14 @@ routes.patch('/users')
 routes.get('/users', UserController.getUsers)
 routes.get('/users/:user_id', UserController.getUserById)
 
-routes.post('/products/:user_id', ProductController.createProduct)
-routes.patch('/products/:user_id/:product_id', ProductController.updateProduct)
+routes.post('/products/:user_id', authenticate, ProductController.createProduct)
+routes.patch('/products/:user_id/:product_id', authenticate, ProductController.updateProduct)
 routes.get('/products', ProductController.getProducts)
 routes.get('/products/:user_id', ProductController.getUserProducts)
 routes.get('/products/id/:product_id', ProductController.getProductById)
-routes.delete('/products/:user_id/:product_id', ProductController.deleteProduct)
+routes.delete('/products/:user_id/:product_id', authenticate, ProductController.deleteProduct)
 
 
-routes.post('/carts/:user_id', CartController.createCart)
+routes.post('/carts/:user_id', authenticate, CartController.createCart)
 routes.get('/carts/:user_id', CartController.getUserCart)
 routes.get('/carts/:user_id/:cart_id', CartController.getCart)
